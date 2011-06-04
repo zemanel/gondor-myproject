b2fdd94ac402a23149896f9959be829e6d0e03fc
José Moreira <zemanel@zemanel.eu>
2011-06-05 00:57:15 +0100

    - cached the hello_world view

--- myproject/hello_world/views.py
+++ myproject/hello_world/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render_to_response
+from django.views.decorators.cache import cache_page
 
 from hello_world.models import Message
 
+@cache_page(3600)
 def say_hello(request, template="hello_world/hello.html"):
     message = get_object_or_404(Message, pk=1)
     return render_to_response(template, {
